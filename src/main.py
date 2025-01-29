82e9bd58e404acee6d0dd329c5752597a602b3e4
kalea <kc@class-docker>
2025-01-29 19:48:27 +0000

    update time module

--- src/main.py
+++ src/main.py
@@ -1,5 +1,2 @@
-from datetime import datetime
-now = datetime.now()
-current_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
-
-print(current_time_str) #datetime module from python found on google
+import time
+print(time.time())
