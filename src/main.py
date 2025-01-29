9d2c3ca9219f7590e0c2849530461e288dbbe2ed
kalea <kc@class-docker>
2025-01-29 19:53:15 +0000

    Revert "update time module"
    This reverts commit 82e9bd58e404acee6d0dd329c5752597a602b3e4.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
-import time
-print(time.time())
+from datetime import datetime
+now = datetime.now()
+current_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+print(current_time_str) #datetime module from python found on google
