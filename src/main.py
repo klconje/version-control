b1c65714aa929005fd1f43edb97f388d6f24959f
klconje <kconje@uci.edu>
2025-02-05 16:19:29 -0800

    Update main.py
    Attempt 1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 now = datetime.now()
 current_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+with open(path,'a') as file:
+  file.write(current_time_str)
 
-print(current_time_str) #datetime module from python found on google
+print('/repo/version-control/docs/version.md') #datetime module from python found on google
