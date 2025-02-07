bd462aa23acc2f8043cf407e133b54055d5abfbf
klconje <kconje@uci.edu>
2025-02-07 00:04:32 -0800

    Update main.py
    Replacing print() with write_time()

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ def write_time(path):
   current_time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") #datetime module from python found on google
   with open(path,'a') as file:
     file.write(current_time_str)
-print('/repo/version-control/docs/version.md') 
+write_time('/repo/version-control/docs/version.md')
